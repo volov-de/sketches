da40c860241ec9d59411f79d3d737c1d229b6900
V_Volov <volow15122001@gmail.com>
2025-08-12 18:11:38 +0300

    add routes_storage

--- ClickHouse/ClickHouse.sql
+++ ClickHouse/ClickHouse.sql
@@ -119,4 +119,119 @@ ORDER BY partition;
 --201611	6529
 
 
+Создайте агрегационную суммирующую таблицу, которая считает
+по каждому городу вылета количество вылетов (total_flights)
+и количество вылетов с опозданием (delayed_flights). 
+Назовите ее flights_aggregates
 
+CREATE TABLE startde_student_data.vj_volov_airports
+(
+    airport_code String,
+    airport_name String,
+    city String,
+    longitude Float64,
+    latitude Float64,
+    timezone String
+)
+ENGINE = MergeTree
+ORDER BY airport_code;
+
+INSERT INTO startde_student_data.vj_volov_airports
+SELECT *
+FROM postgresql(
+    'startde.postgres.karpov.courses:5432',
+    'demo',
+    'airports',
+    'vj_volov',
+    '304f084e76a1a85f33eadaf7671813587a9c41535137e8a6ac2bdc43fb858cc7',
+    'bookings'
+);
+
+--Проверка
+SELECT * FROM startde_student_data.vj_volov_airports LIMIT 5;
+
+
+CREATE TABLE startde_student_data.vj_volov_flights_aggregates
+(
+    departure_airport String,
+    total_flights UInt64,
+    delayed_flights UInt64
+)
+ENGINE = SummingMergeTree
+ORDER BY (departure_airport)
+SETTINGS index_granularity = 8192;
+
+INSERT INTO startde_student_data.vj_volov_flights_aggregates
+SELECT
+    f.departure_airport,
+    COUNT(*) AS total_flights,
+    SUM(CASE 
+        WHEN f.actual_departure > f.scheduled_departure THEN 1 
+        ELSE 0 
+    END) AS delayed_flights
+FROM startde_student_data.vj_volov_flights f
+GROUP BY f.departure_airport;
+
+select * from startde_student_data.vj_volov_flights_aggregates
+
+--departure_airport, total_flights, delayed_flights
+--AAQ	849	751
+--ASF	961	855
+--NFG	283	251
+--NUX	3282	2897
+--SKX	792	695
+
+
+
+
+Чтобы эффективно планировать рейсы и улучшать клиентский сервис, вашей команде аналитиков требуется 
+централизованное место хранения всей информация о маршрутах.
+Создайте материализованное представление routes со следующими полями с запросом на базе таблицы flights:
+flight_no (первичный ключ) — номер рейса
+departure_airport — код аэропорта вылета
+arrival_airport — код аэропорта прилета
+aircraft_code — код самолета
+duration — продолжительность полета, которая будет рассчитана как разница между запланированными временами вылета и прилета
+
+CREATE TABLE startde_student_data.vj_volov_routes_storage
+(
+    flight_no String,
+    departure_airport String,
+    arrival_airport String,
+    aircraft_code String,
+    duration UInt32
+)
+ENGINE = ReplacingMergeTree  -- чтобы при дублях оставалась последняя строка
+ORDER BY flight_no;
+
+CREATE MATERIALIZED VIEW startde_student_data.vj_volov_routes
+TO startde_student_data.vj_volov_routes_storage
+AS
+SELECT
+    flight_no,
+    any(departure_airport) AS departure_airport,
+    any(arrival_airport) AS arrival_airport,
+    any(aircraft_code) AS aircraft_code,
+    any(toUInt32(scheduled_arrival - scheduled_departure)) AS duration
+FROM startde_student_data.vj_volov_flights
+GROUP BY flight_no;
+
+-- Создаём бэкап
+CREATE TABLE IF NOT EXISTS startde_student_data.vj_volov_flights_backup
+ENGINE = MergeTree ORDER BY flight_id
+AS SELECT * FROM startde_student_data.vj_volov_flights;
+
+-- Очищаем
+TRUNCATE TABLE startde_student_data.vj_volov_flights;
+
+-- Вставляем обратно — сработает MV
+INSERT INTO startde_student_data.vj_volov_flights
+SELECT * FROM startde_student_data.vj_volov_flights_backup;
+
+SELECT * FROM startde_student_data.vj_volov_routes_storage limit 5;
+
+--PG0001	UIK	SGC	CR2	8400
+--PG0002	SGC	UIK	CR2	8400
+--PG0003	IWA	AER	CR2	7800
+--PG0004	AER	IWA	CR2	7800
+--PG0005	DME	PKV	CN1	7500
